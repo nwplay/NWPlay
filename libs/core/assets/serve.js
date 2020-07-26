c0fcf0f34ba62f26a07769c085ef17ad95fddaf3
Allan Amstadt <allan@amstadt.de>
2020-07-26 15:03:04 +0200

    Added error log for plugin dev-server

--- libs/core/assets/serve.js
+++ libs/core/assets/serve.js
@@ -2,7 +2,7 @@
 const { watch } = require('rollup');
 const { config, pkg } = require('./config');
 const serve = require('rollup-plugin-serve');
-console.log(`Serving ${pkg.name} (${pkg.version})`)
+console.log(`Serving ${pkg.name} (${pkg.version}) ----`)
 
 config.plugins.push(serve({
   port: 8065,
@@ -12,11 +12,16 @@ config.plugins.push(serve({
 }));
 
 const watcher = watch(config);
-
 watcher.on('change', (e) => {
-  console.log(`${e} changed.`)
+  console.log(`"${e}" changed.\nRebuilding now`)
+});
+
+watcher.on('event', (e) => {
+  if(e.code === 'ERROR') {
+    console.error(e.error.message);
+  }
 });
 
 watcher.on('restart', (e) => {
-  console.log(`Reloaded`);
+  console.log(`Rebuilding done`);
 });
