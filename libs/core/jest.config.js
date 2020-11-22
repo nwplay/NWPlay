4fcb37edf52589b5a184f622b80c3ff19eb42c6a
Allan Amstadt <allanamstadt@Allans-MacBook-Pro-Arbeit.local>
2020-11-22 20:09:33 +0100

    Angular 11 upgrade

--- libs/core/jest.config.js
+++ libs/core/jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  name: 'core',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/core',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'core',
 };
